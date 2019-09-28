544597e82408d37f4942b9ab5fed8e6d514c6df2
Yazan Lpizra <ylpizra@my.bridgeport.edu>
2019-09-28 14:54:57 -0400

    Publishing to gh-pages (publish.sh)

--- deploy.ps1
+++ deploy.ps1
@@ -1,22 +1,37 @@
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+#!/bin/sh
 
-# Build the project.
-hugo -t hugo-tufte # if using a theme, replace with `hugo -t <YOURTHEME>`
+# colors reference
+# http://web.theurbanpenguin.com/adding-color-to-your-output-from-c/
 
-# Go To Public folder
-# cd public
+$color_red = "\033[1;31m"
+$color_green = "\033[1;32m"
+$color_end = "\033[0m"
 
-# Add changes to git.
-git add --all
-
-# Commit changes.
-if([string]::IsNullOrEmpty($Args[0])){
-    git commit -m "rebuilding site $(date)"
-}else{
-	git commit -m $Args[0]
+if ([string]::IsNullOrEmpty($("git status -s"))){ 
+    printf "$color_red The working directory is dirty. Please commit any pending changes. $color_end\n"
+    exit 1;
 }
 
-# Push source and build repos.
-git push origin master
+printf "$color_green Deleting old publication $color_end\n"
+Remove-Item -LiteralPath "public" -Force -Recurse
+mkdir public
+git worktree prune
+Remove-Item -LiteralPath ".git/worktrees/public/" -Force -Recurse
 
-echo done
+printf "$color_green Checking out gh-pages branch into public $color_end\n"
+git worktree add -B gh-pages public upstream/gh-pages
+
+printf "$color_green Removing existing files $color_end\n"
+Remove-Item -LiteralPath "public/*" -Force -Recurse
+
+printf "$color_green Generating site $color_end\n"
+hugo
+
+printf "$color_green Updating gh-pages branch $color_end\n"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+#echo "Pushing to github"
+git push --all
+cd ..
+
+printf "$color_green Done $color_end\n"
